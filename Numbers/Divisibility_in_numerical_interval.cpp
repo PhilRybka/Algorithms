8795b9933746fc26ec802eb8482ef7f3d0a43bd6
Phil Rybka <filrybka@gmail.com>
2016-03-12 16:31:50 +0100

    Update and rename Divisibility in <a,b> to Divisibility_in_numerical_interval.cpp

--- Numbers/Divisibility_in_numerical_interval.cpp
+++ Numbers/Divisibility_in_numerical_interval.cpp
@@ -1,11 +1,11 @@
+//Prints numbers from a <a,b> numerical interval which are divisible by c
+
 #include <iostream>
 using namespace std;
 
 int main(){
     int a,b,c;
-    
     cin>>a>>b>>c;
-    
     for(int i=a;i<=b;i++){
         if(i%c==0) cout<<i<<endl;
         }
