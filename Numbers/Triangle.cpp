82bb1f5f6ce7c8f38bd1c74a2dd88b56a2f1a09c
Phil Rybka <filrybka@gmail.com>
2016-03-16 22:42:09 +0100

    Update Triangle.cpp

--- Numbers/Triangle.cpp
+++ Numbers/Triangle.cpp
@@ -2,7 +2,6 @@
 
 bool triangle (int a, int b, int c)
 {
-  if(a>0 && b>0 && c>0 && (a+b)>c && (a+c)>b && (b+c)>a) return true;
-  return false;
+  return (a>0 && b>0 && c>0 && a+b>&& a+c>b && b+c>a);
 }
 
