30ff27790deb469e4fcc9510a1ef9a06bbb1ce19
Phil Rybka <filrybka@gmail.com>
2016-03-23 20:07:31 +0100

    Update Triangle.cpp

--- Numbers/Triangle.cpp
+++ Numbers/Triangle.cpp
@@ -1,6 +1,6 @@
 //a, b, c are lengths of a triangle's sides, the functions checks if a triangle with these sides is valid
 
-bool triangle (int a, int b, int c)
+bool is_valid_triangle (int a, int b, int c)
 {
   return (a>0 && b>0 && c>0 && a+b>&& a+c>b && b+c>a);
 }
