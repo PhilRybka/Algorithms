c2db2ece968270b77af1ea2a86992c5f4b156172
Phil Rybka <filrybka@gmail.com>
2016-03-23 20:18:14 +0100

    Update cm2inches.cpp

--- Conversions/cm2inches.cpp
+++ Conversions/cm2inches.cpp
@@ -1,6 +1,6 @@
 //Convers centimeters to inches 
 
 double cm2inches(double cm){
-    double c=0.3937;              //for inches to centimeters c=2.54
-    return cm*c;
+    return cm*0.3937;           //for inches to centimeters c=2.54
+    
 }
